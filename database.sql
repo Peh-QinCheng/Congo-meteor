de0ec752da1e9dcf4fb80c038ec52089fc0467c4
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-11-29 11:42:09 +0800

    Ctrl +Alt + L

--- database.sql
+++ database.sql
@@ -4,19 +4,20 @@ USE `bookstore`;
 DROP TABLE IF EXISTS `Books`;
 
 CREATE TABLE `Books` (
-  `ISBN` char(13) NOT NULL,
-  `title` char(50) DEFAULT NULL,
-  `author` char(50) DEFAULT NULL,
-  `publisher` char(50) DEFAULT NULL,
-  `year` date DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `bkformat` char(20) DEFAULT NULL,
-  `keywords` char(100) DEFAULT NULL,
-  `subject` char(100) DEFAULT NULL,
-  `copies` int(11) DEFAULT NULL,
+  `ISBN`      CHAR(13) NOT NULL,
+  `title`     CHAR(50)  DEFAULT NULL,
+  `author`    CHAR(50)  DEFAULT NULL,
+  `publisher` CHAR(50)  DEFAULT NULL,
+  `year`      DATE      DEFAULT NULL,
+  `price`     DOUBLE    DEFAULT NULL,
+  `bkformat`  CHAR(20)  DEFAULT NULL,
+  `keywords`  CHAR(100) DEFAULT NULL,
+  `subject`   CHAR(100) DEFAULT NULL,
+  `copies`    INT(11)   DEFAULT NULL,
   PRIMARY KEY (`ISBN`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
 
 # Dump of table Customers
@@ -25,15 +26,16 @@ CREATE TABLE `Books` (
 DROP TABLE IF EXISTS `Customers`;
 
 CREATE TABLE `Customers` (
-  `login` char(30) NOT NULL,
-  `password` char(30) NOT NULL,
-  `name` char(30) DEFAULT NULL,
-  `credit_num` char(30) DEFAULT NULL,
-  `address` char(100) DEFAULT NULL,
-  `phone_num` int(11) DEFAULT NULL,
+  `login`      CHAR(30) NOT NULL,
+  `password`   CHAR(30) NOT NULL,
+  `name`       CHAR(30)  DEFAULT NULL,
+  `credit_num` CHAR(30)  DEFAULT NULL,
+  `address`    CHAR(100) DEFAULT NULL,
+  `phone_num`  INT(11)   DEFAULT NULL,
   PRIMARY KEY (`login`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
 
 # Dump of table Feedbacks
@@ -42,17 +44,18 @@ CREATE TABLE `Customers` (
 DROP TABLE IF EXISTS `Feedbacks`;
 
 CREATE TABLE `Feedbacks` (
-  `login` char(30) NOT NULL DEFAULT '',
-  `ISBN` char(13) NOT NULL DEFAULT '',
-  `score` int(11) DEFAULT NULL,
-  `content` char(200) DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  PRIMARY KEY (`login`,`ISBN`),
+  `login`   CHAR(30) NOT NULL DEFAULT '',
+  `ISBN`    CHAR(13) NOT NULL DEFAULT '',
+  `score`   INT(11)           DEFAULT NULL,
+  `content` CHAR(200)         DEFAULT NULL,
+  `date`    DATE              DEFAULT NULL,
+  PRIMARY KEY (`login`, `ISBN`),
   KEY `ISBN` (`ISBN`),
   CONSTRAINT `feedbacks_ibfk_1` FOREIGN KEY (`login`) REFERENCES `Customers` (`login`),
   CONSTRAINT `feedbacks_ibfk_2` FOREIGN KEY (`ISBN`) REFERENCES `Books` (`ISBN`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
 
 # Dump of table Invoices
@@ -61,15 +64,16 @@ CREATE TABLE `Feedbacks` (
 DROP TABLE IF EXISTS `Invoices`;
 
 CREATE TABLE `Invoices` (
-  `invoiceid` int(11) NOT NULL,
-  `login` char(30) DEFAULT NULL,
-  `order_date` date DEFAULT NULL,
-  `order_status` char(20) DEFAULT NULL,
+  `invoiceid`    INT(11) NOT NULL,
+  `login`        CHAR(30) DEFAULT NULL,
+  `order_date`   DATE     DEFAULT NULL,
+  `order_status` CHAR(20) DEFAULT NULL,
   PRIMARY KEY (`invoiceid`),
   KEY `login` (`login`),
   CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`login`) REFERENCES `Customers` (`login`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
 
 # Dump of table Orders
@@ -78,16 +82,17 @@ CREATE TABLE `Invoices` (
 DROP TABLE IF EXISTS `Orders`;
 
 CREATE TABLE `Orders` (
-  `invoiceid` int(11) NOT NULL DEFAULT '0',
-  `ISBN` char(13) NOT NULL DEFAULT '',
-  `price` double DEFAULT NULL,
-  `copies` int(11) DEFAULT NULL,
-  PRIMARY KEY (`invoiceid`,`ISBN`),
+  `invoiceid` INT(11)  NOT NULL DEFAULT '0',
+  `ISBN`      CHAR(13) NOT NULL DEFAULT '',
+  `price`     DOUBLE            DEFAULT NULL,
+  `copies`    INT(11)           DEFAULT NULL,
+  PRIMARY KEY (`invoiceid`, `ISBN`),
   KEY `ISBN` (`ISBN`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`invoiceid`) REFERENCES `Invoices` (`invoiceid`),
   CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`ISBN`) REFERENCES `Books` (`ISBN`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
 
 # Dump of table Ratings
@@ -96,14 +101,16 @@ CREATE TABLE `Orders` (
 DROP TABLE IF EXISTS `Ratings`;
 
 CREATE TABLE `Ratings` (
-  `login` char(30) NOT NULL DEFAULT '',
-  `ISBN` char(13) NOT NULL DEFAULT '',
-  `rater_login` char(30) NOT NULL DEFAULT '',
-  `rating` int(11) DEFAULT NULL,
-  PRIMARY KEY (`login`,`ISBN`,`rater_login`),
+  `login`       CHAR(30) NOT NULL DEFAULT '',
+  `ISBN`        CHAR(13) NOT NULL DEFAULT '',
+  `rater_login` CHAR(30) NOT NULL DEFAULT '',
+  `rating`      INT(11)           DEFAULT NULL,
+  PRIMARY KEY (`login`, `ISBN`, `rater_login`),
   KEY `rater_login` (`rater_login`),
   KEY `ISBN` (`ISBN`),
   CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`login`) REFERENCES `Customers` (`login`),
   CONSTRAINT `ratings_ibfk_2` FOREIGN KEY (`rater_login`) REFERENCES `Customers` (`login`),
   CONSTRAINT `ratings_ibfk_3` FOREIGN KEY (`ISBN`) REFERENCES `Books` (`ISBN`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;

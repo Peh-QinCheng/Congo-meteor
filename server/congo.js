b996a0bf9fc1395632d295a76189137a11824016
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-11-29 11:45:16 +0800

    Refactor files

--- server/congo.js
+++ server/congo.js
@@ -3,45 +3,34 @@ books = new MysqlSubscription('allBooks');
 users = new MysqlSubscription('allUsers');
 
 
-if (Meteor.isServer) {
-  var liveDb = new LiveMysql({
+var liveDb = new LiveMysql({
     host: 'localhost',
     port: 3407,
     user: 'root',
     password: '',
     database: 'bookstore'
-  });
+});
 
-  var closeAndExit = function() {
+var closeAndExit = function () {
     liveDb.end();
     process.exit();
-  };
-  // Close connections on hot code push
-  process.on('SIGTERM', closeAndExit);
-  // Close connections on exit (ctrl + c)
-  process.on('SIGINT', closeAndExit);
+};
+// Close connections on hot code push
+process.on('SIGTERM', closeAndExit);
+// Close connections on exit (ctrl + c)
+process.on('SIGINT', closeAndExit);
 
-  Meteor.publish('allBooks', function() {
+Meteor.publish('allBooks', function () {
     return liveDb.select(
-      'SELECT * FROM Books',
-      [ { table: 'Books' } ]
+        'SELECT * FROM Books',
+        [{table: 'Books'}]
     );
-  });
+});
 
-  Meteor.publish('allUsers', function() {
+Meteor.publish('allUsers', function () {
     return liveDb.select(
-      'SELECT * FROM Customers',
-      [ { table: 'Customers' } ]
+        'SELECT * FROM Customers',
+        [{table: 'Customers'}]
     );
-  });
+});
 
-  Meteor.methods({
-    'new_user': function(email, password) {
-      check(email, String);
-      check(password, String);
-      liveDb.db.query(
-        'INSERT INTO Customers (login, password) VALUES ( ? , ?)', [ email, password ]);
-    }
-  });
-
-}

67fd332c59afa494d03509cf10ec9557f89b0548
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-11-29 22:37:35 +0800

    Remove comments, spacing

--- routes.js
+++ routes.js
@@ -3,23 +3,20 @@ Router.configure({
 });
 
 Router.route('/', function () {
-  this.render('register');
+    this.render('register');
 });
 
 Router.route('/home', function () {
-  this.render('home');
+    this.render('home');
 });
 
 
 Router.route('/book/:isbn', {
-  //subscription: function () {
-  //  return new MysqlSubscription('bookByISBN', this.params.isbn)
-  //},
-  data: function () {
-    var ISBN = this.params.isbn;
-    return {ISBN: ISBN};
-  },
-  action: function () {
-    this.render('bookDetails');
-  }
+    data: function () {
+        var ISBN = this.params.isbn;
+        return {ISBN: ISBN};
+    },
+    action: function () {
+        this.render('bookDetails');
+    }
 });

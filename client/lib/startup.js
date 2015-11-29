97c93f3f90cbfd34d68be1486147c43df8dd3d58
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-11-30 03:14:21 +0800

    Initialize customer

--- client/lib/startup.js
+++ client/lib/startup.js
@@ -2,4 +2,9 @@ Meteor.startup(function () {
     var cart = localStorage.getItem(KEY_CURRENT_CART);
     cart = cart ? JSON.parse(cart) : {}; // initialize as empty array if null
     Session.set(KEY_CURRENT_CART, cart)
+
+    var customer = localStorage.getItem(KEY_CURRENT_CUSTOMER);
+    if(customer){
+        Session.set(KEY_CURRENT_CUSTOMER, customer)
+    }
 });

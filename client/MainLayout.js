e54075ffe3f897dfe737709649f8a5268072eb2f
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-11-29 12:02:41 +0800

    ;

--- client/MainLayout.js
+++ client/MainLayout.js
@@ -1,10 +1,10 @@
 Template.MainLayout.helpers({
     loggedIn: function () {
         if (typeof(Storage) === "undefined") {
-            console.log('local storage not supported, not gonna care for this project')
+            console.log('local storage not supported, not gonna care for this project');
             return false
         }
-        console.log(Boolean(localStorage.getItem(KEY_CURRENT_CUSTOMER)))
+        console.log(Boolean(localStorage.getItem(KEY_CURRENT_CUSTOMER)));
         return Boolean(localStorage.getItem(KEY_CURRENT_CUSTOMER));
     },
     currentUser: function () {

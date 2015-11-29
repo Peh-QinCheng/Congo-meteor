cc105a905f31e34748dd648f56086c52dd38b436
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-11-29 12:03:07 +0800

    Remove try catch as it doesn't save mysql from crashing meteor server

--- server/methods/registerCustomer.js
+++ server/methods/registerCustomer.js
@@ -1,13 +1,10 @@
 Meteor.methods({
     'registerCustomer': function (email, password) {
-        try {
-            check(email, String);
-            check(password, String);
-            liveDb.db.query(
-                'INSERT INTO Customers (login, password) VALUES ( ? , ?)', [email, password]);
-            return email;
-        } catch (err) {
-            console.log('Error while inserting new customer: ', err)
-        }
+        check(email, String);
+        check(password, String);
+        liveDb.db.query(
+            'INSERT INTO Customers (login, password) VALUES ( ? , ?)', [email, password]);
+        return email;
+
     }
 });

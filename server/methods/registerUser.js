28f2098cfd90ea3791712b9aa7e088d3d6295117
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-11-29 11:43:40 +0800

    Ctrl + Alt + L

---
+++ server/methods/registerUser.js
@@ -0,0 +1,9 @@
+Meteor.methods({
+    'registerUser': function (email, password) {
+        check(email, String);
+        check(password, String);
+        liveDb.db.query(
+            'INSERT INTO Customers (login, password) VALUES ( ? , ?)', [email, password]);
+        return email;
+    }
+});

862fcc403b5634271d8e73a15fdf24eac221d753
Jia Hao <jiahaog@gmail.com>
2015-12-01 00:30:23 +0800

    Implement retrieval of helpfulness rating for feedback

--- client/views/book/book.js
+++ client/views/book/book.js
@@ -65,7 +65,26 @@ Template.bookDetails.events({
                 return;
             }
         });
-    },
+    }
+});
+
+Template.bookFeedback.onCreated(function () {
+    var currentFeedback = this.data;
+    this.currentFeedbackRating = new MysqlSubscription('feedbackRating', currentFeedback.login, Template.currentData().ISBN);
+});
+
+Template.bookFeedback.helpers({
+    rating: function () {
+        Template.instance().currentFeedbackRating.depend();
+        var queryResult = Template.instance().currentFeedbackRating.reactive()[0];
+        if (queryResult) {
+            return queryResult['SUM(rating)'] || '-'
+        }
+        return '-'
+    }
+});
+
+Template.bookFeedback.events({
     'click button.feedback-score': function (event) {
         var changeScoreBy = parseInt(event.target.value);
         var newScore = this.score + changeScoreBy;
--- server/publications/feedbackPub.js
+++ server/publications/feedbackPub.js
@@ -16,3 +16,20 @@ Meteor.publish('bookFeedbacks', function (isbn) {
         }
     }]);
 });
+
+Meteor.publish('feedbackRating', function (feedbackOp, isbn) {
+    var table = 'Ratings';
+    var ratingAttribute = 'rating';
+    return liveDb.select(function (esc, escId) {
+        return `SELECT SUM(${ratingAttribute}) FROM ${escId(table)} WHERE login=${esc(feedbackOp)} and ISBN=${esc(isbn)}`;
+    }, [{
+        table: table,
+        condition: function (row, newRow) {
+            // Only refresh the results when the row matching the specified id is
+            // changed.
+            return row.id === id
+                    // On UPDATE queries, newRow must be checked as well
+                || (newRow && newRow.id === id);
+        }
+    }]);
+});

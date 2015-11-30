b8c5f0fa65627d9157958220155ff324eb7b0217
Jia Hao <jiahaog@gmail.com>
2015-11-30 17:02:27 +0800

    Implement feedback front end

--- client/views/book/book.js
+++ client/views/book/book.js
@@ -10,6 +10,21 @@ Template.bookDetails.helpers({
         if (books.ready()) {
             return books.length === 0 ? 'No book found!' : books[0].title;
         }
+    },
+    bookFeedbacks: function () {
+        var no = 5;
+        var result = [];
+        for (var i = 0; i < no; i++) {
+            result.push({
+                login: 'user1',
+                ISBN: '0319',
+                score: 5,
+                content: 'Feedback here',
+                date: new Date()
+            });
+        };
+
+        return result;
     }
 });
 
@@ -29,5 +44,26 @@ Template.bookDetails.events({
             }
         }
         Session.set(KEY_CURRENT_CART, cart)
+    },
+    'submit form': function (event) {
+        event.preventDefault();
+        var feedbackText = event.target.feedbackValue.value;
+
+        //  Meteor.call('submitFeed', email, password, function (err, res) {
+        //     if (err) {
+        //         console.log('OH DAMN SOMETHING REALLY BAD HAPPENED');
+        //         return
+        //     }
+        //     if (res === null) {
+        //         alert("Username is already taken!");
+        //         return
+        //     }
+        //     localStorage.setItem(KEY_CURRENT_CUSTOMER, res);
+        // });
+    },
+    'click button.feedback-score': function (event) {
+        var changeScoreBy = parseInt(event.target.value);
+        var newScore = this.score + changeScoreBy;
+        console.log('score clicked:', this.login, this.ISBN, newScore);
     }
 });

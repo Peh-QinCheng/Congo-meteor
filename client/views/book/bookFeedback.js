267be9c9629e387fc099eeedad2f165738b9e2b9
Jia Hao <jiahaog@gmail.com>
2015-12-01 11:32:34 +0800

    Implement rating of feedback

--- client/views/book/bookFeedback.js
+++ client/views/book/bookFeedback.js
@@ -16,8 +16,19 @@ Template.bookFeedback.helpers({
 
 Template.bookFeedback.events({
     'click button.feedback-score': function (event) {
-        var changeScoreBy = parseInt(event.target.value);
-        var newScore = this.score + changeScoreBy;
-        console.log('score clicked:', this.login, this.ISBN, newScore);
+        var newRating = parseInt(event.target.value);
+        var feedbackLogin = this.login;
+        var feedbackIsbn = this.ISBN;
+        var raterLogin = localStorage.getItem(KEY_CURRENT_CUSTOMER);
+        Meteor.call('addFeedbackRating', feedbackLogin, feedbackIsbn, raterLogin, newRating, function (error, queryError) {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            if (queryError) {
+                console.error(queryError);
+                return;
+            }
+        });
     }
 });
--- server/methods/feedback.js
+++ server/methods/feedback.js
@@ -7,9 +7,28 @@ Meteor.methods({
         check(scoreNumber, Number);
         var response = Async.runSync(function (done) {
             liveDb.db.query(
-
                 'INSERT INTO Feedbacks (login, ISBN, content, score) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE content=VALUES(content), score=VALUES(score)', 
                 [login, isbn, content, scoreNumber], function (error, results, fields) {
+                    if (error) { 
+                        done(error);
+                        return;
+                    }
+
+                    done();
+                });
+        });
+        return response.error;
+    },
+    'addFeedbackRating': function (login, isbn, raterLogin, raterRating) {
+        check(login, String);
+        check(isbn, String);
+        check(raterLogin, String);
+        var scoreNumber = parseInt(raterRating);
+        check(scoreNumber, Number);
+        var response = Async.runSync(function (done) {
+            liveDb.db.query(
+                'INSERT INTO Ratings (login, ISBN, rater_login, rating) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE rating=VALUES(rating)', 
+                [login, isbn, raterLogin, scoreNumber], function (error, results, fields) {
                     if (error) { 
                         done(error);
                         return;

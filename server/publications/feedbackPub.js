bd083b1a746b1e07adb49afb354744d737d82ea4
Jia Hao <jiahaog@gmail.com>
2015-12-01 15:06:20 +0800

    Fix bug in query where unrated feedback will not be displayed

--- server/publications/feedbackPub.js
+++ server/publications/feedbackPub.js
@@ -1,6 +1,5 @@
 Meteor.publish('bookFeedbacks', function (isbn, sortBy, limit) {
     var table = 'Feedbacks';
-
     var sortParam;
     switch (sortBy) {
         case 'date':
@@ -14,28 +13,8 @@ Meteor.publish('bookFeedbacks', function (isbn, sortBy, limit) {
             break;
     }
 
-    let limitQuery;
-    if (limit) {
-        limitQuery = `LIMIT ${limit};`;
-    } else {
-        limitQuery = ';';
-    }
-
     return liveDb.select(function (esc, escId) {
-        return (
-            `SELECT
-              Feedbacks.login, ISBN, score, content, date, ratings.avg_rating
-            FROM
-              Feedbacks,
-              (SELECT AVG(rating) AS avg_rating, login FROM Ratings WHERE ISBN=${esc(isbn)} GROUP BY login) as ratings
-            WHERE
-              Feedbacks.ISBN=${esc(isbn)} AND Feedbacks.login=ratings.login
-            ORDER BY
-              ${sortParam} DESC
-            #   avg_rating DESC
-            ${limitQuery}
-            `
-        );
+        return feedbackQuery(isbn, sortParam, limit);
     }, [{
         table: table,
         condition: function (row, newRow) {
@@ -47,3 +26,52 @@ Meteor.publish('bookFeedbacks', function (isbn, sortBy, limit) {
         }
     }]);
 });
+
+function escapeString(str) {
+    return `'${str}'`;
+}
+
+function feedbackQuery(isbn, sortParam, limit) {
+    let escapedIsbn = escapeString(isbn);
+
+    let limitQuery;
+    if (limit) {
+        limitQuery = `LIMIT ${limit};`;
+    } else {
+        limitQuery = ';';
+    }
+
+    return `
+        SELECT
+          *
+        FROM
+          (
+            # feedback that has been rated
+            SELECT
+              Feedbacks.login, ISBN, score, content, date, ratings.avg_rating
+            FROM
+              Feedbacks,
+              (SELECT AVG(rating) AS avg_rating, login FROM Ratings GROUP BY login, ISBN) as ratings
+            WHERE
+              Feedbacks.login=ratings.login
+            UNION
+              # Feedback that has not been rated
+              SELECT
+                login,
+                ISBN,
+                score,
+                content,
+                date,
+                '-' # indicates that it has not been rated
+            FROM
+              Feedbacks
+            WHERE
+              login NOT IN (SELECT login FROM Ratings)
+          ) AS subquery
+        WHERE
+          ISBN=${escapedIsbn}
+        ORDER BY
+          ${sortParam} DESC
+        ${limitQuery}
+    `;
+}

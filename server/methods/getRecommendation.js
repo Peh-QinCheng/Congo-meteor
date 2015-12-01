c9634d2902ee11ebafcc777d9320c0d9c2c31d87
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-12-01 21:34:17 +0800

    Add missing login parameter

--- server/methods/getRecommendation.js
+++ server/methods/getRecommendation.js
@@ -1,23 +1,21 @@
 Meteor.methods({
-    getRecommendation: function (isbn) {
+    getRecommendation: function (isbn, login) {
         var table = 'Books';
         return liveDb.select(function (esc, escId) {
-            //SELECT * FROM BOOKS WHERE ISBN IN ( SELECT ISBN FROM Orders o , Invoices i WHERE o.invoiceid = i.invoiceid)
             return (
                 // Orders by other users (who have bought the same book) on other books,
                 // grouped by books, ordered by diff number of users who ordered the same book
-                'SELECT ISBN, COUNT(DISTINCT login) AS count' +
-                'FROM Orders o JOIN Invoices i ON o.invoiceid = i.invoiceid' +
-                'WHERE i.login IN (' +
-                    // Other users who have bought the same book
-                    'SELECT i2.login ' +
-                    'FROM Orders o2 JOIN Invoices i2 ON o2.invoiceid = i2.invoiceid' +
-                "WHERE o2.ISBN = $(esc(isbn)) AND i2.login != $(esc(login))" +
-                ') ' +
-                'AND ISBN != $(esc(isbn))' +
-                'GROUP BY ISBN' +
-                'ORDER BY count DESC'
-            );
+                `
+                SELECT ISBN, COUNT(DISTINCT login) AS count
+                FROM Orders o JOIN Invoices i ON o.invoiceid = i.invoiceid
+                WHERE i.login IN (
+                    SELECT i2.login FROM Orders o2 JOIN Invoices i2 ON o2.invoiceid = i2.invoiceid
+                    WHERE o2.ISBN = ${esc(isbn)} AND i2.login != ${esc(login)}
+                    )
+                AND ISBN != $(esc(isbn))
+                GROUP BY ISBN ORDER BY count DESC
+                `
+            )
         }, [{
             table: table,
             condition: function (row, newRow) {

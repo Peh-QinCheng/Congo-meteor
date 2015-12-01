599e15c52a2304daea4b2b589972e0ebdc50606f
Qin Cheng <pqc92@hotmail.com>
2015-12-01 23:47:19 +0800

    added method to insert new book

--- server/methods/updateBook.js
+++ server/methods/updateBook.js
@@ -3,5 +3,9 @@ Meteor.methods({
       check(quantity, Number);
       check(ISBN, String);
       liveDb.db.query('UPDATE books SET copies = ? WHERE ISBN = ?', [ quantity, ISBN ]);
-    }  
+    },
+
+    'makeNewBook': function (quantity, ISBN, author, publisher, year, price, bkformat, keywords, subject, title) {
+        liveDb.db.query('INSERT INTO Books (copies, ISBN, author, publisher, year, price, bkformat, keywords, subject, title) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [quantity, ISBN, author, publisher, year, price, bkformat, keywords, subject, title]);
+    }
 });

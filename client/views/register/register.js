43e769940c59217ea1bf1ba5722e64991497ca2b
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-12-01 21:34:05 +0800

    Registration validation

--- client/views/register/register.js
+++ client/views/register/register.js
@@ -3,6 +3,18 @@ Template.register.events({
         events.preventDefault();
         var email = events.target.email.value;
         var password = events.target.password.value;
+
+        if (email.trim().search(/^[a-z0-9_-]{3,16}$/i) === -1) {
+            alert('Username can only contain alphanumeric characters, hyphens and underscore and be within 3 to 16 characters')
+            return;
+        }
+
+        if (password.length < 5) {
+            if (!confirm('Password must have 6 or more characters! Are you sure about this!?')) {
+                return
+            }
+        }
+
         Meteor.call('registerCustomer', email, password, function (err, res) {
             if (err) {
                 console.log('OH DAMN SOMETHING REALLY BAD HAPPENED');

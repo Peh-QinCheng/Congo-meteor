09a1fcf3b5bd4b0a56fba51feb4880d93c38ca20
Qin Cheng <pqc92@hotmail.com>
2015-12-02 16:18:02 +0800

    added event on sortform submit

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,4 +1,7 @@
-books = new MysqlSubscription('allBooks');
+Template.home.onCreated(function () {
+  this.filteredSortedBooks = new MysqlSubscription('filteredSortedBooks', null, null);
+  Session.set('LOL', true);
+});
 
 Template.home.helpers({
     currentCustomer: function () {
@@ -6,6 +9,28 @@ Template.home.helpers({
     },
 
     books: function () {
-      return books.reactive();
+      var makeMeReactive = Session.get('LOL');
+      var filteredSortedBooks = Template.instance().filteredSortedBooks.reactive();
+      return filteredSortedBooks;
     }
 });
+
+Template.home.events ({
+  'submit form': function (events) {
+    events.preventDefault();
+    var query_params = {
+      author: events.target.author.value,
+      publisher: events.target.publisher.value,
+      subject: events.target.subject.value,
+      title: events.target.bookTitle.value
+    };
+    var sort = events.target.sort.value;
+    var filteredSortedBooks = Template.instance().filteredSortedBooks;
+    filteredSortedBooks.stop();
+
+    Template.instance().filteredSortedBooks = new MysqlSubscription('filteredSortedBooks', query_params, sort);
+    Session.set('LOL', !Session.get('LOL'));
+  }
+
+});
+

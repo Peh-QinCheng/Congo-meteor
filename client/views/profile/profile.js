a2d3beb19f79cf01df7f398beccb0fd83b7b1b33
Jia Hao <jiahaog@gmail.com>
2015-12-02 12:21:16 +0800

    Implement css and seeing users own feedbacks and ratings

--- client/views/profile/profile.js
+++ client/views/profile/profile.js
@@ -1,6 +1,8 @@
 Template.profile.onCreated(function () {
     this.data.profile = new MysqlSubscription('customerProfile', Template.currentData().login);
     this.data.invoices = new MysqlSubscription('customerInvoice', Template.currentData().login);
+    this.data.feedbackHistory = new MysqlSubscription('feedbackHistory', Template.currentData().login);
+    this.data.ratedFeedback = new MysqlSubscription('ratedFeedback', Template.currentData().login);
 });
 
 Template.userInfo.helpers({
@@ -22,6 +24,30 @@ Template.orderHistory.helpers({
         if (invoices.ready()) {
             console.log(invoices);
             return invoices.length === 0 ? ['No invoices found!'] : invoices;
+        } else {
+            return []
+        }
+    }
+});
+
+Template.feedbackHistory.helpers({
+    feedbackHistory: function (context) {
+        var invoices = context.feedbackHistory;
+        invoices.depend();
+        if (invoices.ready()) {
+            return invoices.length === 0 ? ['No feedback history found!'] : invoices;
+        } else {
+            return []
+        }
+    }
+});
+
+Template.ratedFeedback.helpers({
+    ratedFeedback: function (context) {
+        var invoices = context.ratedFeedback;
+        invoices.depend();
+        if (invoices.ready()) {
+            return invoices.length === 0 ? ['No rated feedback found!'] : invoices;
         } else {
             return []
         }

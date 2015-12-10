f1b9d6425962c240b5c7b3edb25e5d021db7b06b
Jia Hao <jiahaog@gmail.com>
2015-12-11 04:09:09 +0800

    Fix wrong criteria, rated fedback should be ranked according to helpfulness

--- server/publications/ratingsPub.js
+++ server/publications/ratingsPub.js
@@ -1,5 +1,23 @@
 Meteor.publish('ratedFeedback', function (login) {
     return liveDb.select(function (esc, escId) {
-        return `SELECT * FROM ratings WHERE rater_login=${esc(login)}`
+        //return `SELECT * FROM ratings WHERE rater_login=${esc(login)}`
+        return `
+            SELECT
+              AVG(rating) as avg_rating, ISBN
+            FROM
+              ratings
+            WHERE
+              ISBN IN ( #All books that I have rated feedbacks
+                SELECT
+                  ISBN
+                FROM
+                  ratings
+                WHERE
+                  rater_login='${login}'
+              )
+            GROUP BY
+              ISBN
+            ORDER BY
+              avg_rating DESC`
     }, [{table: 'ratings'}]);
 });

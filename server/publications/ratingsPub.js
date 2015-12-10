600b07a66b68d827fd6e089c5c91810d71c8cbd4
Jia Hao <jiahaog@gmail.com>
2015-12-11 04:30:43 +0800

    Fix query, no need for aggregation

--- server/publications/ratingsPub.js
+++ server/publications/ratingsPub.js
@@ -3,21 +3,13 @@ Meteor.publish('ratedFeedback', function (login) {
         //return `SELECT * FROM ratings WHERE rater_login=${esc(login)}`
         return `
             SELECT
-              AVG(rating) as avg_rating, ISBN
+              *
             FROM
               ratings
             WHERE
-              ISBN IN ( #All books that I have rated feedbacks
-                SELECT
-                  ISBN
-                FROM
-                  ratings
-                WHERE
-                  rater_login='${login}'
-              )
-            GROUP BY
-              ISBN
+              rater_login='sadsadad'
             ORDER BY
-              avg_rating DESC`
+              rating DESC;
+           `
     }, [{table: 'ratings'}]);
 });

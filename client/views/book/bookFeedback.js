e1880c60b8f0639f1ba29918c172dea65dd2a8af
Jia Hao <jiahaog@gmail.com>
2015-12-13 04:42:45 +0800

    Remove log statement

--- client/views/book/bookFeedback.js
+++ client/views/book/bookFeedback.js
@@ -1,12 +1,10 @@
 Template.bookFeedback.events({
     'click button.book-feedback-score': function (event) {
         event.preventDefault();
-        console.log(event.target);
         var newRating = parseInt(event.target.value);
         var feedbackLogin = this.login;
         var feedbackIsbn = this.ISBN;
         var raterLogin = Session.get(KEY_CURRENT_CUSTOMER);
-        console.log(feedbackLogin, feedbackIsbn, raterLogin, newRating);
         Meteor.call('addFeedbackRating', feedbackLogin, feedbackIsbn, raterLogin, newRating, function (error, queryError) {
             if (error) {
                 console.error(error);

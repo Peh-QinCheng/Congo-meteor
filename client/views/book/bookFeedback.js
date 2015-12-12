58c012817ec33c646a6276f0a5ec87b49b10e12c
Jia Hao <jiahaog@gmail.com>
2015-12-12 23:01:10 +0800

    Implement nice view of feedback

--- client/views/book/bookFeedback.js
+++ client/views/book/bookFeedback.js
@@ -1,9 +1,12 @@
 Template.bookFeedback.events({
-    'click button.feedback-score': function (event) {
+    'click button.book-feedback-score': function (event) {
+        event.preventDefault();
+        console.log(event.target);
         var newRating = parseInt(event.target.value);
         var feedbackLogin = this.login;
         var feedbackIsbn = this.ISBN;
         var raterLogin = Session.get(KEY_CURRENT_CUSTOMER);
+        console.log(feedbackLogin, feedbackIsbn, raterLogin, newRating);
         Meteor.call('addFeedbackRating', feedbackLogin, feedbackIsbn, raterLogin, newRating, function (error, queryError) {
             if (error) {
                 console.error(error);
